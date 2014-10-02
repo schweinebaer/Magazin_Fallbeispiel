fc32c5ef1c7d6faf5f1ee1af5a16721ad2fdb24f
Benedikt Breitschopf <benedikt.breitschopf@gmail.com>
2014-10-02 23:50:08 +0200

    Erste Klassen hinzugefügt

---
+++ src/de/spiel/magazin/Magazin.java
@@ -0,0 +1,26 @@
+package de.spiel.magazin;
+
+
+public class Magazin {
+	
+	private Standort standort;
+	private Werbung eigenwerbung;
+	private Werbung fremdwerbung;
+	private Marktanteil marktanteil;
+	private double kostenPapierProHeft;
+	private double kostenDruckProHeft;
+	private double PersonalkostenProHeft;
+	private double gesamtkostenProHeft;
+	private int auflage;
+	private double gesamtkostenAuflage;
+	private double einnahmenAuflage;
+	private double gewinnAuflage;
+	
+	private int mitarbeiter;
+
+	public void Magazin(Standort s, Marktanteil m) {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
