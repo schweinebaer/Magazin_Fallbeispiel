958dbbcc1fa05d0187845a3922a1d08973a4abd1
Viviane <Viviane@Viviane>
2014-10-02 22:59:04 +0200

    Vivi PC hat Main erstellt

---
+++ src/GithubTestKlasse.java
@@ -0,0 +1,13 @@
+public class GithubTestKlasse {
+
+	public GithubTestKlasse() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main (String[]args){
+		int i = 1;
+		int ii = 2;
+		int iii = i+ii;
+		System.out.println(iii);
+	}
+
+}
