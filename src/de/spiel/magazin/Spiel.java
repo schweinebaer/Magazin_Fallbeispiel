1e712b851bfd29eb873738c755e236a3f7a661a8
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-03 14:46:25 +0200

    Update Spieler und Spiel

--- src/de/spiel/magazin/Spiel.java
+++ src/de/spiel/magazin/Spiel.java
@@ -1,13 +1,14 @@
 package de.spiel.magazin;
 
 import java.io.*;
+import java.util.Vector;
 
 public class Spiel {
 	/*
 	 * @author Pascal Pronobis
 	 */
 	
-	private static Spieler[] spieler;
+	private static Vector<Spieler> spieler;
 	private static Spieler aktuellerSpieler;
 	private static int aktuelleRunde;
 	private static int maxRunde;
@@ -25,20 +26,55 @@ public class Spiel {
 		
 		aktuelleEingabe = reader.readLine();
 		
-		while(aktuelleEingabe.isNumeric())
-		writer.println("");
-		writer.println("");
-		writer.println("");
+		while(!isNumeric(aktuelleEingabe)){
+			aktuelleEingabe.toUpperCase();
+			
+			if(aktuelleEingabe.equals("BEENDEN")){
+				System.exit(0);
+			}
+			
+			writer.println("Eingabe keine gültige Zahl. Bitte neu eingeben.");
+			aktuelleEingabe = reader.readLine();
+		}
+		
+		maxRunde = Integer.parseInt(aktuelleEingabe);
+		
+		aktuelleEingabe = reader.readLine();
+		aktuelleEingabe.toUpperCase();
+		splitAktuelleEingabe = aktuelleEingabe.split(" ");
+		
+		while(!aktuelleEingabe.equals("SPIELER_FERTIG")){
+			if(splitAktuelleEingabe[0].equals("SPIELER_HINZUFUEGEN")){
+				Spieler einzelnerSpieler = new Spieler(splitAktuelleEingabe[1]);
+				spieler.add(einzelnerSpieler);
+			} else if(aktuelleEingabe.equals("HILFE")){
+				
+			} else if(aktuelleEingabe.equals("BEENDEN")){
+				System.exit(0);
+			} else {
+				writer.println("Invalide Eingabe. Wiederholen Sie bitte Ihre Eingabe oder suchen Sie die Hilfe unter HILFE auf.");
+			}
+			
+			aktuelleEingabe = reader.readLine();
+			aktuelleEingabe.toUpperCase();
+			splitAktuelleEingabe = aktuelleEingabe.split(" ");
+		}
 		
 		while(aktuelleRunde < maxRunde){
-			for(int i = 0; i < spieler.length; i++){
-				aktuellerSpieler = spieler[i];
+			for(int i = 0; i < spieler.capacity(); i++){
+				aktuellerSpieler = spieler.elementAt(i);
 				writer.println("Nächste Aktionen für " + aktuellerSpieler.getName());
 				
 				aktuelleEingabe = reader.readLine().toUpperCase();
 				splitAktuelleEingabe = aktuelleEingabe.split(" ");
 				
-				simulieren(splitAktuelleEingabe[0], Double.parseDouble(splitAktuelleEingabe[1]));
+				if(aktuelleEingabe.equals("HILFE")){
+					
+				} else if(aktuelleEingabe.equals("BEENDEN")){
+					System.exit(0);
+				} else {
+					simulieren(splitAktuelleEingabe[0], Double.parseDouble(splitAktuelleEingabe[1]));
+				}
 			}
 
 			aktuelleRunde++;
@@ -52,7 +88,15 @@ public class Spiel {
 		
 	}
 	
-	private static boolean isNumeric(){
-		return false;
+	private static boolean isNumeric(String z){
+		int zahl = 0;
+		
+		try {
+			zahl = Integer.parseInt(z);
+		} catch(NumberFormatException e){
+			return false;
+		}
+		
+		return true;
 	}
 }
--- src/de/spiel/magazin/Spieler.java
+++ src/de/spiel/magazin/Spieler.java
@@ -3,6 +3,10 @@ package de.spiel.magazin;
 public class Spieler {
 	private String name;
 	
+	public Spieler(String name){
+		this.name = name;
+	}
+	
 	public String getName(){
 		return name;
 	}
