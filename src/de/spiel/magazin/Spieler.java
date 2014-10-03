e35225310a1da5ecc8b2e9c56c36b0806c9b25ac
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-03 17:28:15 +0200

    up

--- src/de/spiel/magazin/Spieler.java
+++ src/de/spiel/magazin/Spieler.java
@@ -1,7 +1,14 @@
 package de.spiel.magazin;
 
+import java.util.Vector;
+
 public class Spieler {
 	private String name;
+	private Magazin magazin;
+	private Vector<Kredit> kredite;
+	private Vector<Bericht> berichte;
+	private double kapital;
+	private double ek;
 	
 	public Spieler(String name){
 		this.name = name;
@@ -9,5 +16,33 @@ public class Spieler {
 	
 	public String getName(){
 		return name;
+	}
+	
+	public Magazin getMagazin() {
+		return null;
+	}
+	
+	public void setMagazin(String standort){
+		
+	}
+	
+	public Kredit getKredit(){
+		return null;
+	}
+	
+	public void setKredit(int betrag){
+		
+	}
+	
+	public void ermittleEK(){
+		
+	}
+	
+	public double getEK(){
+		return 0;
+	}
+	
+	public void simulieren(String art, double betrag){
+		
 	}
 }
