9ceed4da538c9b19f4b4a290a91d7012a6974ac9
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-03 16:48:28 +0200

    added

--- src/de/spiel/magazin/Spiel.java
+++ src/de/spiel/magazin/Spiel.java
@@ -21,6 +21,7 @@ public class Spiel {
 	public static void main(String[] args) throws IOException {
 		reader = new BufferedReader(new InputStreamReader(System.in));
 		writer = new PrintWriter(System.out, true);
+		int i = 1;
 		
 		writer.println("Willkommen beim Planspiel!");
 		writer.println("Bitte geben Sie eine maximale Rundenanzahl ein!");
