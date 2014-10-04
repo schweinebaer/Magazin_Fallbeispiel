154917fbe12880b70b0130ebfd7e0176c4827b2f
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-04 11:45:58 +0200

    Update

--- src/de/spiel/magazin/Standort.java
+++ src/de/spiel/magazin/Standort.java
@@ -3,31 +3,62 @@ package de.spiel.magazin;
 import java.util.Vector;
 
 
-public class Standort {
+public abstract class Standort {
+	private final static String standort1 = "München";
+	private final static double magazinkosten1 = 5000.0;
+	private final static int maxMitarbeiter1 = 50;
+	
+	private final static String standort2 = "Berlin";
+	private final static double magazinkosten2 = 2000;
+	private final static int maxMitarbeiter2 = 40;
+	
+	private final static String standort3 = "Walldorf";
+	private final static double magazinkosten3 = 500;
+	private final static int maxMitarbeiter3 = 20;
+	
 	private Standorttyp name;
 	private double magazinkosten;
 	private int maxMitarbeiter;
 
 	
-public Standort(Standorttyp name) {
-	this.name = name;
-	setMagazinkosten(name.getMagazinkosten());
-	setMaxMitarbeiter(name.getMaxMitarbeiter());
+	public Standort(Standorttyp name) {
+		this.name = name;
+		setMagazinkosten(name.getMagazinkosten());
+		setMaxMitarbeiter(name.getMaxMitarbeiter());
+	}
+	
+	public static Object[] getStandort(int i){
+		Object[] o = new Object[3];
+		
+		if(i == 1){
+			o[0] = standort1;
+			o[1] = magazinkosten1;
+			o[2] = maxMitarbeiter1;
+		} else if(i == 2){
+			o[0] = standort2;
+			o[1] = magazinkosten2;
+			o[2] = maxMitarbeiter2;			
+		} else if(i == 3){
+			o[0] = standort2;
+			o[1] = magazinkosten2;
+			o[2] = maxMitarbeiter2;
+		} else {
+			//falscher Standort
+			return null;
+		}
+		
+		return o;
 	}
 
+	private void setMagazinkosten(double magazinkosten) {
+		this.magazinkosten = magazinkosten;
+	}
 
-private void setMagazinkosten(double magazinkosten) {
-	this.magazinkosten = magazinkosten;
-	
-}
+	private void setMaxMitarbeiter(int maxMitarbeiten) {
+		this.maxMitarbeiter = maxMitarbeiten;
+	}	
 
-private void setMaxMitarbeiter(int maxMitarbeiten) {
-	this.maxMitarbeiter = maxMitarbeiten;
-	
-}	
-
-public int getMaxMitarbeiter(int maxMitabeiter) {
-	return maxMitarbeiter;
-}
-
+	public int getMaxMitarbeiter(int maxMitabeiter) {
+		return maxMitarbeiter;
+	}
 }
