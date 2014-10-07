57ca69e4de2ed3fa178c734b97b3ede4c53103b7
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-07 12:04:59 +0200

    Update Spiel

--- src/de/spiel/magazin/Spiel.java
+++ src/de/spiel/magazin/Spiel.java
@@ -77,6 +77,7 @@ public class Spiel {
 		
 		while(aktuelleRunde < maxRunde){
 			for(int i = 0; i < spieler.size(); i++){
+				//genaue Berechnungen durchführen!!
 				zufallsereignis = Zufallsereignis.berechneZufallsEreignis();
 				writer.println(zufallsereignis[0]);
 				
