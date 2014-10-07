c7c49961e7d5d1c0150aa44ec59b31bbab8db10f
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-07 12:04:10 +0200

    Update Zufallsereignis

--- src/de/spiel/magazin/Zufallsereignis.java
+++ src/de/spiel/magazin/Zufallsereignis.java
@@ -1,11 +1,10 @@
 package de.spiel.magazin;
 
 public class Zufallsereignis {
-	private static int ereignisRandomZahl;
 	private static ZufallsereignisDaten daten = new ZufallsereignisDaten(); 
 		
 	public static Object[] berechneZufallsEreignis(){
-		ereignisRandomZahl = (int) Math.random() * 14;
+		int ereignisRandomZahl = (int) (Math.random() * 14);
 		
 		return daten.getEreignis(ereignisRandomZahl);
 	}
