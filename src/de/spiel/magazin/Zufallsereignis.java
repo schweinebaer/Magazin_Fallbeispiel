ea38a8ed107c2f1da4fe52a75bc12825aa55d623
Pascal Pronobis <pascal.facebooketc@gmail.com>
2014-10-07 11:55:01 +0200

    Zufallsereignisse hinzugefügt

--- src/de/spiel/magazin/Zufallsereignis.java
+++ src/de/spiel/magazin/Zufallsereignis.java
@@ -1,7 +1,13 @@
 package de.spiel.magazin;
 
 public class Zufallsereignis {
-	public Zufallsereignis(){
+	private static int ereignisRandomZahl;
+	private static Object[] ereignisString = ereignisString = new String[4];
+	private static ZufallsereignisDaten daten = new ZufallsereignisDaten(); 
 		
+	public static Object[] berechneZufallsEreignis(){
+		ereignisRandomZahl = (int) Math.random() * 14;
+		
+		return daten.getEreignis(ereignisRandomZahl);
 	}
 }
